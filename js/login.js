04efed2e09882a30b43dcf95550dcb10afff04d1
Pardis <154790380+pardis-mdd@users.noreply.github.com>
2024-04-14 21:56:45 +0330

    Update login.js

--- js/login.js
+++ js/login.js
@@ -9,3 +9,11 @@ sign_up_btn.addEventListener("click", () => {
 sign_in_btn.addEventListener("click", () => {
   container.classList.remove("sign-up-mode");
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  document
+    .getElementById("login-button")
+    .addEventListener("click", function () {
+      window.location.href = "products.html";
+    });
+});
